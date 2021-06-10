ef18111c7eba810059922a6b28f4d23267597ffc
tjdls111 <tjdls111@naver.com>
2021-06-10 16:51:25 +0900

    퀴즈박스

--- scr/main.js
+++ scr/main.js
@@ -1,6 +1,14 @@
+"use strict";
 var menu = document.querySelector('.menu');
 var desBtn = document.querySelector('.menu-one');
 var quizBtn = document.querySelector('.menu-two');
 menu === null || menu === void 0 ? void 0 : menu.addEventListener('click', function (event) {
-    console.log(event);
+    if (event.target) {
+        if (event.target.className == 'menu-one') {
+            location.href = 'des.html';
+        }
+        else if (event.target.className == 'menu-two') {
+            location.href = 'quiz.html';
+        }
+    }
 });
--- scr/main.ts
+++ scr/main.ts
@@ -4,7 +4,15 @@ const menu:html=document.querySelector('.menu');
 const desBtn:html=document.querySelector('.menu-one');
 const quizBtn:html=document.querySelector('.menu-two');
 
-menu?.addEventListener('click',(event)=>{
-    console.log(event);
+menu?.addEventListener('click',(event:MouseEvent)=>{
+    if (event.target){
+        if (event.target.className=='menu-one'){
+           location.href='des.html';
+        }
+        else if (event.target.className=='menu-two'){
+            location.href='quiz.html';
+        }
+    }
+
     
 });
