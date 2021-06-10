5ca9137f7ec792a5d5dc4b8315e4df7ef77f3b14
tjdls111 <tjdls111@naver.com>
2021-06-10 18:52:27 +0900

    정답입력창,값

--- scr/quiz/quiz.js
+++ scr/quiz/quiz.js
@@ -4,19 +4,19 @@ export var buttonStart = document.querySelector('.button-start');
 var Quiz = /** @class */ (function () {
     function Quiz(quiz) {
         var quizZone = document.createElement('div');
-        quizZone.innerHTML = "<section class=\"quiz-box\">\n        <button class=\"close\">&times;</button>\n        <div id=\"quiz-box_body\">" + quiz + "</div>\n        <button class=\"quiz_submit\">\uC815\uB2F5 \uD655\uC778\uD558\uAE30</button>\n    </section>\n        ";
+        quizZone.innerHTML = "<section class=\"quiz-box\">\n        <button class=\"close\">&times;</button>\n        <div id=\"quiz-box_body\">" + quiz + "</div>\n        <input type=\"text\" class='quiz_answer'></input>\n        <button class=\"quiz_submit\">\uC815\uB2F5 \uD655\uC778\uD558\uAE30</button>\n    </section>\n        ";
         main === null || main === void 0 ? void 0 : main.appendChild(quizZone);
-        console.log('quize');
-        /*const closeBtn=template.querySelector('.close')! as HTMLElement;
-        closeBtn.onclick=()=>{
-            console.log('닫기');
-            
-        }
-        const submitBtn=template.querySelector('.quiz_submit')! as HTMLElement;
-        submitBtn.onclick=()=>{
-            console.log('제출하기');
-            
-        };*/
+        var closeBtn = quizZone.querySelector('.close');
+        closeBtn.onclick = function (event) {
+            main === null || main === void 0 ? void 0 : main.removeChild(quizZone);
+        };
+        var submitBtn = quizZone.querySelector('.quiz_submit');
+        var answer = quizZone.querySelector('.quiz_answer');
+        submitBtn.onclick = function (event) {
+            if (answer.value == "정답") {
+                main === null || main === void 0 ? void 0 : main.appendChild(quizZone);
+            }
+        };
     }
     return Quiz;
 }());
--- scr/quiz/quiz.ts
+++ scr/quiz/quiz.ts
@@ -11,25 +11,27 @@ export class Quiz{
         quizZone.innerHTML=`<section class="quiz-box">
         <button class="close">&times;</button>
         <div id="quiz-box_body">${quiz}</div>
+        <input type="text" class='quiz_answer'></input>
         <button class="quiz_submit">정답 확인하기</button>
     </section>
         `;
   
         main?.appendChild(quizZone);
-        console.log('quize');
 
-        /*const closeBtn=template.querySelector('.close')! as HTMLElement;
-        closeBtn.onclick=()=>{
-            console.log('닫기');
+        const closeBtn=quizZone.querySelector('.close')! as HTMLElement;
+        closeBtn.onclick=(event)=>{
+            main?.removeChild(quizZone);
+        };
+
+        const submitBtn=quizZone.querySelector('.quiz_submit')! as HTMLElement;
+        const answer=quizZone.querySelector('.quiz_answer')! as HTMLElement;
+        submitBtn.onclick=(event)=>{
+            if(answer.value=="정답"){
+                main?.appendChild(quizZone);
+            }
             
         }
-        const submitBtn=template.querySelector('.quiz_submit')! as HTMLElement;
-        submitBtn.onclick=()=>{
-            console.log('제출하기');
-            
-        };*/
-
-
+        
         
 
     }
