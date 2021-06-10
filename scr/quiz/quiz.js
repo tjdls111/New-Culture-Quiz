5769827b2949ac6d2803a90f421ca9050b0ec800
tjdls111 <tjdls111@naver.com>
2021-06-10 18:05:41 +0900

    quiz존 보이게

--- scr/quiz/quiz.js
+++ scr/quiz/quiz.js
@@ -1,10 +1,12 @@
 import { clickBtn } from "../button.js";
-export var main = document.querySelector('main');
+export var main = document.querySelector('#game-zone');
 export var buttonStart = document.querySelector('.button-start');
 var Quiz = /** @class */ (function () {
     function Quiz(quiz) {
-        var template = document.createElement('template');
-        template.innerHTML = "<section class=\"quiz-box\">\n        <button class=\"close\">&times;</button>\n        <div id=\"quiz-box_body\">" + quiz + "</div>\n        <button class=\"quiz_submit\">\uC815\uB2F5 \uD655\uC778\uD558\uAE30</button>\n    </section>\n        ";
+        var quizZone = document.createElement('div');
+        quizZone.innerHTML = "<section class=\"quiz-box\">\n        <button class=\"close\">&times;</button>\n        <div id=\"quiz-box_body\">" + quiz + "</div>\n        <button class=\"quiz_submit\">\uC815\uB2F5 \uD655\uC778\uD558\uAE30</button>\n    </section>\n        ";
+        main === null || main === void 0 ? void 0 : main.appendChild(quizZone);
+        console.log('quize');
         /*const closeBtn=template.querySelector('.close')! as HTMLElement;
         closeBtn.onclick=()=>{
             console.log('닫기');
@@ -15,8 +17,6 @@ var Quiz = /** @class */ (function () {
             console.log('제출하기');
             
         };*/
-        main === null || main === void 0 ? void 0 : main.appendChild(template);
-        console.log('quize');
     }
     return Quiz;
 }());
--- scr/quiz/quiz.ts
+++ scr/quiz/quiz.ts
@@ -1,21 +1,22 @@
 import { clickBtn } from "../button.js";
 
 type html=null|HTMLElement;
-export const main:html=document.querySelector('main');
+export const main:html=document.querySelector('#game-zone');
 export const buttonStart:html=document.querySelector('.button-start');
 
 export class Quiz{
 
     constructor(quiz:string){
-        const template=document.createElement('template');
-        template.innerHTML=`<section class="quiz-box">
+        const quizZone=document.createElement('div');
+        quizZone.innerHTML=`<section class="quiz-box">
         <button class="close">&times;</button>
         <div id="quiz-box_body">${quiz}</div>
         <button class="quiz_submit">정답 확인하기</button>
     </section>
         `;
   
-        
+        main?.appendChild(quizZone);
+        console.log('quize');
 
         /*const closeBtn=template.querySelector('.close')! as HTMLElement;
         closeBtn.onclick=()=>{
@@ -28,8 +29,7 @@ export class Quiz{
             
         };*/
 
-        main?.appendChild(template);
-        console.log('quize');
+
         
 
     }
